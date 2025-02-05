253f488d867463141e2b20d6edcbcc5ffdc1e926
Maha-lakshmi30 <115567792+Maha-lakshmi30@users.noreply.github.com>
2025-02-05 16:42:51 +0530

    Add files via upload

---
+++ day1/Commandline.java
@@ -0,0 +1,11 @@
+public class Commandline
+{
+    public static void main(String args[])
+   {
+ System.out.println("Total no.of.Arguments "+args.length);
+  System.out.println("first argument "+args[0]);
+ System.out.println("Second argument "+args[1]);
+ System.out.println("Third argument "+args[2]);
+   }
+}
+
